225ef5d34681336e4a5346fa56af843b24b38269
Joao2007Pedro <pedropedrinho201617@gmail.com>
2026-04-01 12:33:30 -0300

    Elementos graficos iniciado

---
+++ logica/Aula 5 - elementos-graficos/index.js
@@ -0,0 +1,11 @@
+function stark() {
+    document.querySelector(".fundo").style.backgroundColor="black";
+    document.querySelector(".fundo").style.color="white"
+    document.querySelector(".myImg").src="./imagens/fundo.jpg"
+}
+
+function snow() {
+    document.querySelector(".fundo").style.backgroundColor="white";
+    document.querySelector(".fundo").style.color="black"
+    document.querySelector(".myImg").src="./imagens/fundo2.webp"
+}
